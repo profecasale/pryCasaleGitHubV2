b2078e53fa7906cf976c081bfcfba56414e565ae
profecasale <jcasale@ies21.edu.ar>
2025-08-27 12:35:34 -0300

    agrego un pic con imagen

--- pryCasaleGitHub/frmGitHub.Designer.cs
+++ pryCasaleGitHub/frmGitHub.Designer.cs
@@ -29,30 +29,45 @@
         private void InitializeComponent()
         {
             btnNube = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // btnNube
             // 
-            btnNube.Location = new Point(102, 101);
+            btnNube.Location = new Point(90, 36);
             btnNube.Name = "btnNube";
-            btnNube.Size = new Size(511, 147);
+            btnNube.Size = new Size(511, 60);
             btnNube.TabIndex = 0;
             btnNube.Text = "Me fui a la nube";
             btnNube.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.PLACAdocentes;
+            pictureBox1.Location = new Point(208, 121);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(255, 193);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
             // 
             // frmGitHub
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
             Controls.Add(btnNube);
             Name = "frmGitHub";
             Text = "Registro en GitHub de mi proyecto";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private Button btnNube;
+        private PictureBox pictureBox1;
     }
 }
